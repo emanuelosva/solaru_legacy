ed08ba956f450550cd4a2f1048443a009faa0dc1
emanuelosva <emanuelosva@gmail.com>
2020-05-25 08:01:13 -0500

    e agergo modulo para calcular el costo total del sistema.

--- core/financial/models.py
+++ core/financial/models.py
@@ -1,1 +1,52 @@
 """Models for financial calculations."""
+
+# Local
+from .system_cost import (
+    find_price_inverter,
+    find_price_module,
+    instalation_cost,
+    electrical_bos_cost
+)
+
+
+# Models
+class FinancialCalc:
+    """
+    Class for make all cost calculations.
+    """
+
+    def __init__(self, kwh_cost, module, inverter, system):
+        self.kwh_cost = kwh_cost
+        self.module = module
+        self.number_modules = system[0]
+        self.inverter = inverter
+        self.number_inverters = system[1]
+
+    def calc_cost(self):
+        self.system_cost()
+        self.total_cost()
+
+    def system_cost(self):
+        self.find_cost()
+        self.structure_cost = structure_cost(
+            modules=self.number_modules
+        )
+
+        self.electrical_cost = electrical_bos_cost(
+            modules=self.system[0]
+        )
+
+        self.system_cost_ = (
+            self.electrical_cost +
+            self.structure_cost +
+            self.module_cost * self.number_modules +
+            self.inverter_cost * self.number_inverters
+        )
+
+    def find_cost(self):
+        self.module_cost = find_price_module(self.module)
+        self.inverter_cost = find_price_inverter(self.inverter)
+
+    def total_cost(self):
+        self.instalation_cost_ = instalation_cost(self.system_cost_)
+        self.total_cost_ = self.system_cost_ + self.instalation_cost_
--- core/financial/system_cost.py
+++ core/financial/system_cost.py
@@ -1,1 +1,81 @@
 """Info for photovoltaic system cost."""
+
+# Local
+from core.energy.output_energy import PTC_POWER_MODULE
+
+
+# Typical prices for fotovoltaic components:
+# Prices finded on SunWatts: https://sunwatts.com/
+MODULES = {
+    # model_name : prices in MXN.
+
+    'SunPower_SPR_305_WHT___2009_': 5400,
+}
+
+INVERTERS = {
+    # model_name : prices in MXN.
+
+    'ABB__PVI_CENTRAL_50_US__480V_': 92500,
+    'ABB__TRIO_20_0_TL_OUTD_S1B_US_480__480V_': 39089,
+    'ABB__UNO_8_6_TL_OUTD_S_US_A__240V_': 19055,
+    'ABB__PVI_3_0_OUTD_S_US_A__240V_': 8815,
+    'ABB__MICRO_0_3_I_OUTD_US_240__240V_': 4007,
+}
+
+# Aditional costs, info by NREL: U.S. Solar Photovoltaic System Cost Benchmark: Q1 2018
+STRUCTURE_PRICE = 2  # MXN per Watt installed
+ELCTRICAL_BOS_PRICE = 4  # MXN per Watt installed
+INSTALATION = 0.2  # Percentage of the total cost
+
+# Conversions
+kW_to_W = 1000
+
+
+# Functions
+def find_price_module(module_model):
+    """
+    Returns the price of the module.
+    """
+
+    if module_model in MODULES:
+        return MODULES[module_model]
+    else:
+        raise 'The module does not in database'
+
+
+def find_price_inverter(inverter_model):
+    """
+    Returns the price of the inverter.
+    """
+
+    if inverter_model in INVERTERS:
+        return INVERTERS[inverter_model]
+    else:
+        raise 'The inverter does not in database'
+
+
+def structure_cost(num_modules):
+    """
+    Returns the cost of photovoltaic structure.
+    """
+
+    total_installed_power = num_modules * PTC_POWER_MODULE * kW_to_W
+
+    return STRUCTURE_PRICE * total_installed_power
+
+
+def electrical_bos_cost(num_modules):
+    """
+    Returns the cost of aditional electrical components.
+    """
+
+    total_installed_power = num_modules * PTC_POWER_MODULE * kW_to_W
+
+    return ELCTRICAL_BOS_PRICE * total_installed_power
+
+
+def instalation_cost(system_cost):
+    """
+    Returns the instalation costs.
+    """
+    return system_cost * INSTALATION
