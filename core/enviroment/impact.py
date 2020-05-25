7f8d4b518de976766408448d0941d868b8b758a8
emanuelosva <emanuelosva@gmail.com>
2020-05-24 20:31:12 -0500

    Se creo función para determinar el CO2 eliminado.

--- core/enviroment/impact.py
+++ core/enviroment/impact.py
@@ -2,3 +2,17 @@
 Calculates the positive environmental impact derived 
 from the installation of the proposed photovoltaic system.
 """
+
+
+# Functions
+def co2_evited(anual_energy_by_pv_system):
+    """
+    Returns the anual CO2 eliminated by the emissions
+    reduction derived from photovoltaic system in kilograms of CO2.
+
+    Reference:
+    [1] ""
+    """
+
+    co2_eliminated_per_year = anual_energy_by_pv_system*0.9
+    return co2_eliminated_per_year
