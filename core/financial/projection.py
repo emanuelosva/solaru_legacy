cdad42d3b660d48eced271d3ba7564e3910164c9
emanuelosva <emanuelosva@gmail.com>
2020-05-25 08:44:32 -0500

    Se agrego modulo para calcular la proyección financiera del sistem.

--- core/financial/projection.py
+++ core/financial/projection.py
@@ -1,1 +1,46 @@
 """Financial projection for photovailtic system."""
+
+# Utilities
+import numpy as np
+
+# Local
+from .models import FinancialCalc
+
+
+# Functions
+def financial_calc(energy_by_month, kwh_cost, module, inverter, system):
+    """
+    Returns:
+        - The month money savings of electrical coute derived of pv system.
+        - The investment return time of the pv system.
+    """
+    cost = FinancialCalc(kwh_cost, module, inverter, system)
+    cost.calc_cost()
+    savings = _savings(energy_by_month, kwh_cost)
+    time_return = _investment_return(cost.total_cost_, savings)
+
+    return savings, time_return
+
+
+def _savings(energy_by_month, kwh_cost):
+    """
+    Returns savings by month.
+    """
+
+    savings = {}
+    for month, energy in energy_by_month.items():
+        savings[month] = energy*kwh_cost
+
+    return savings
+
+
+def _investment_return(total_cost, savings):
+    """
+    Returns investment return time.
+    """
+
+    # Simple return
+    anual_savigs = np.sum(list(savings.values()))
+    time = total_cost/anual_savigs
+
+    return time
