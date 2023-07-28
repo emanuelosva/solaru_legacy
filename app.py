37d71c1b2bc6b1ad5cfb5dfd06118ff0ac99ac8a
emanuelosva <emanuelosva@gmail.com>
2023-07-27 21:45:12 -0600

    :rocket: (deploy) Add app entry file for render

---
+++ app.py
@@ -0,0 +1,44 @@
+"""Entry point for Solaru App."""
+
+# Flask
+from flask import render_template, url_for
+
+# Local
+from app import app_factory
+from app.forms import DataToCalcForm
+from app.models import CalcActive
+
+# Create app
+app = app_factory()
+
+
+# Error handlers
+@app.errorhandler(404)
+def not_fount(error):
+    """Return template for not found URL"""
+
+    return render_template('error_404.html', error=error)
+
+
+@app.errorhandler(500)
+def server_error(error):
+    """Return template for not found URL"""
+
+    return render_template('error_500.html', error=error)
+
+
+# Principal view
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    """Principal View."""
+
+    data_form = DataToCalcForm()
+    calc = CalcActive()
+    context = {'data_form': data_form, 'calc': calc}
+
+    if data_form.is_valid():
+        data = data_form.get_data()
+        calc.activate(**data)
+        return render_template('post.html', **context)
+
+    return render_template('index.html', **context)
